f62c5d4f29eff871632f7bb34ddb586f85b9fad4
numa08 <n511287@gmail.com>
2013-11-28 01:17:32 +0900

    動的にはてなブログの内容を取得し、最近の投稿を取得する処理の実装を行いました

--- numa08_net/app/controllers/Application.scala
+++ numa08_net/app/controllers/Application.scala
@@ -1,11 +1,13 @@
 package controllers
 
-import models.hatena.HatenaBlog
-import models.hatena.HatenaBlogCreator
-import play.api._
-import play.api.mvc._
-import play.api.libs.concurrent.Execution.Implicits._
 import scala.concurrent.Future
+import models.hatena._
+import play.api._
+import play.api.libs.concurrent.Execution.Implicits._
+import play.api.mvc._
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
+
 
 object Application extends Controller {
 
@@ -13,6 +15,27 @@ object Application extends Controller {
     val futureImple = Future(new HatenaBlogCreator().create)
     futureImple.map{ hatena => 
     	val content = views.html.index(hatena)
+    	Ok(content)
+    }
+  }
+  
+  def hatenaBlogFeeds = Action.async{
+    implicit val hatenaEntryWrites: Writes[HatenaEntry] = (
+      (__ \ "title").write[String] and
+      (__ \ "url").write[String])(unlift(HatenaEntry.unapply))
+      
+    val futureImple = Future({
+      val entriesJson = new HatenaEntryAcuire().acuire
+    		  								   .map(Json.toJson(_))
+      JsArray(entriesJson)
+    })
+    futureImple.map(Ok(_))
+  }
+  
+  def hatena = Action.async{
+    val futureImple = Future(new HatenaBlogCreator().create)
+    futureImple.map{ hatena =>
+    	val content = views.html.hatenablog(hatena)
     	Ok(content)
     }
   }
--- numa08_net/app/models/hatena/HatenaBlog.scala
+++ numa08_net/app/models/hatena/HatenaBlog.scala
@@ -1,11 +1,16 @@
 package models.hatena
 
+import scala.collection.JavaConversions._
 import org.jsoup.Jsoup
 import models.Content
 import models.ContentCreator
+import java.net.URL
 
 case class HatenaBlog(title : String, url : String, favicon : String) extends Content
+case class HatenaEntry(title : String, url : String)
+object HatenaBlog {
 
+}
 class HatenaBlogCreator extends ContentCreator {
   
   def create : HatenaBlog = {
@@ -16,5 +21,22 @@ class HatenaBlogCreator extends ContentCreator {
     val favicon = "img/icon/hatena.png"
     val hatenaBlog = HatenaBlog(title, url, favicon)
     hatenaBlog
+  }
+}
+
+class HatenaEntryAcuire {
+  
+  def acuire : List[HatenaEntry] = {
+    val url = new URL("http://numa08.hateblo.jp/feed")
+    // hatena blog feeds's return "Content-Type: application/atom+xml" in reponse header
+    // jsoup does not parse Mime-type like that.
+    val feeds = Jsoup.parse(url.openStream(), "UTF-8", url.toString())
+    				 .getElementsByTag("entry")
+    				 .map(elem => {
+    				   val title = elem.getElementsByTag("title")(0).text()
+    				   val link  = elem.getElementsByTag("link")(0).attr("href")
+    				   HatenaEntry(title, link)
+    				 }).toList
+    feeds
   }
 }
