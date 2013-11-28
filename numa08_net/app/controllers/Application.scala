322e1c10e327d049e1ec90b4899910a4b580a5b4
numa08 <n511287@gmail.com>
2013-11-29 02:06:50 +0900

    ソーシャル系のコンテンツを表すクラスを分離し、Viewで利用する

--- numa08_net/app/controllers/Application.scala
+++ numa08_net/app/controllers/Application.scala
@@ -1,42 +1,41 @@
 package controllers
 
 import scala.concurrent.Future
+
 import models.hatena._
+import models.twitter.TwitterCreator
+import models.foursquare.FourSquareCreator
+import models.blogger.BloggerCreator
+
 import play.api._
-import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc._
-import play.api.libs.functional.syntax._
-import play.api.libs.json._
+import play.api.libs.concurrent.Execution.Implicits._
 
 
 object Application extends Controller {
 
-  def index = Action.async{
-    val futureImple = Future(new HatenaBlogCreator().create)
-    futureImple.map{ hatena => 
-    	val content = views.html.index(hatena)
+  def index = Action.async {
+    val futureImple = Future{
+      val creators = new TwitterCreator() ::
+    	  			 new HatenaBlogCreator() ::
+    	  			 new FourSquareCreator() ::
+    	  			 new BloggerCreator() ::
+    	  			 Nil
+      val contents = creators.map(_.create)
+      contents
+    }
+    futureImple.map { socials =>
+    	val content = views.html.index(socials)
     	Ok(content)
     }
   }
   
-  def hatenaBlogFeeds = Action.async{
-    implicit val hatenaEntryWrites: Writes[HatenaEntry] = (
-      (__ \ "title").write[String] and
-      (__ \ "url").write[String])(unlift(HatenaEntry.unapply))
-      
-    val futureImple = Future({
-      val entriesJson = new HatenaEntryAcuire().acuire
-    		  								   .map(Json.toJson(_))
-      JsArray(entriesJson)
-    })
-    futureImple.map(Ok(_))
-  }
-  
-  def hatena = Action.async{
-    val futureImple = Future(new HatenaBlogCreator().create)
-    futureImple.map{ hatena =>
-    	val content = views.html.hatenablog(hatena)
-    	Ok(content)
-    }
-  }
+  def hatena = TODO
+//    Action.async{
+//    val futureImple = Future(new HatenaBlogCreator().create)
+//    futureImple.map{ hatena =>
+//    	val content = views.html.hatenablog(hatena)
+//    	Ok(content)
+//    }
+//  }
 }
--- numa08_net/app/models/Content.scala
+++ numa08_net/app/models/Content.scala
@@ -5,3 +5,11 @@ abstract class Content
 abstract class ContentCreator {
   def create : Content
 }
+
+class SocialContent
+case class InternalSocialContent(title : String, favicon : String, link : String) extends SocialContent
+case class ExternalSocialContent(title : String, favicon : String, link : String) extends SocialContent
+
+trait SocialContentCreator {
+  def create : SocialContent
+}
