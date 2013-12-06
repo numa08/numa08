df9bf8e21b0c12aa4da6c73913d3990811f0660a
numa08 <n511287@gmail.com>
2013-12-07 00:27:34 +0900

    Twitterのファビコンを修正

--- numa08_net/app/models/twitter/Twitter.scala
+++ numa08_net/app/models/twitter/Twitter.scala
@@ -8,7 +8,7 @@ class TwitterCreator extends SocialContentCreator {
   
   def create : SocialContent = {
     val twitter = ExternalSocialContent("@numa08",
-    									"https://twitter.com/images/resources/twitter-bird-16x16.png",
+    									"https://dev.twitter.com/sites/default/files/images_documentation/bird_blue_16.png",
     									"http://twitter.com/numa08/")
 	twitter
   }
